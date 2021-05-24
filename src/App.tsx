2fb4cba54cd29fd59d77fb689810e27ea9d77efc
annika.kubaj <70285472+ak0212@users.noreply.github.com>
2021-05-24 16:01:19 +0200

    Added routes, Switch, Links and some basic css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    
+    <Router>
     <div className="App">
       <header className="App-header">
+        <nav>
+          <Link to="/"> Start </Link>
+          <Link to="/galleri"> Galleri </Link>
+          <Link to="/battle"> Battle </Link>
+          <Link to="/statistik"> Statistik </Link>
+          <Link to="/historik"> Historik </Link>
+          nav 
+        </nav>
+      
       </header>
+      <main>
+        före switch 
+        <Switch>
+          <Route path="/galleri"> Galleri </Route>
+          <Route path="/battle"> Battle </Route>
+          <Route path="/statistik"> Statistik </Route>
+          <Route path="/historik"> Historik </Route>
+          <Route path="/"> Välkommen </Route>
+
+        </Switch>
+        efter switch 
+        main 
+      </main>
     </div>
-   
+    </Router>
   );
 }
 
 export default App;
+
