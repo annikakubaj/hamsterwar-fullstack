dbc98c3fb96c8c755ba734655e03eefb4b1691fa
annika.kubaj <70285472+ak0212@users.noreply.github.com>
2021-05-28 12:26:14 +0200

    Started to work with validation on form

--- src/components/history/History.tsx
+++ src/components/history/History.tsx
@@ -1,21 +1,61 @@
 import { useState } from 'react'
+import './Form.css'
 
 const History = () => {
-	const [inputText, setInputText] = useState ('default')
+	const [name, setName] = useState ('');
+	const [name2, setName2] = useState ('');
+	const [controlledText, setControlledText] = useState('')
+	const [controlledText2, setControlledText2] = useState('') 
+	const [nameTouched, setNameTouched] = useState(false)
+
+	let nameIsValid: boolean = (name !== '')
+	let nameClass = ''
+	if ( nameTouched ) {
+		nameClass = (nameIsValid ? 'valid' : 'error')
+	}
+
 	return (
-	<div>
+	<div className="form">
 		<h1> Lägg till ny hamster exempel </h1>
-		<section>
-			<p> 
+		<section> 
+			 
 				<label> Namn: 
-				<input onChange={event =>{
-					console.log('Controlled change', event.target.value);setInputText(event.target.value)
-				}}
-				value={inputText}
+				<input
+				onBlur={() => setNameTouched(true)}
+				 onChange={event =>{
+					console.log('Controlled change', event.target.value);setName(event.target.value)
+					setControlledText(event.target.value)
+				}
+			
+			}
+				value={name}
+				className={nameClass}
 				/>
 				</label>
-			</p>
-		</section>
+				<div className= "message hidden">Error?</div>
+				<br/> 
+				Namn: {controlledText}
+				</section>
+
+				<section> 
+			 
+				<label> Namn: 
+				<input className="valid" onChange={event =>{
+					console.log('Controlled change', event.target.value);setName2(event.target.value)
+					 //toUpperCase fö rsta bokstaven
+					setControlledText2(event.target.value)
+				}
+			
+			}
+				value={name2}
+				/>
+				</label>
+				<div className= "message hidden">Error?</div>
+				<br/> 
+				Namn: {controlledText2}
+				</section>
+			
+			
 	</div>
 )}
 			
